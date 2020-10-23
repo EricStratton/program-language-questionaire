0e00bb28586e50266174ffa2d3739584f1c322e2
Eric Stratton <strattonericj@gmail.com>
2020-10-23 09:39:03 -0700

    add js which succesfully attributes number value to each selection option

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+function add(q1, q2, q3, q4, q5) {
+  return q1 + q2 + q3 + q4 + q5;
+};
+
+
+
+
+$(document).ready(function () {
+  $("form#questionaire").submit(function (event) {
+    event.preventDefault();
+    const q1 = parseInt($("#select1").val());
+    const q2 = parseInt($("input:radio[name=q2]:checked").val());
+    const q3 = parseInt($("#select2").val());
+    const q4 = parseInt($("input:radio[name=q4]:checked").val());
+    const q5 = parseInt($("#select3").val());
+
+    const result = add(q1, q2, q3, q4, q5)
+    $("#test").text(result);
+  });
+});
