c67c809f6f5bc146c06c9b62c101f4cf521e269e
Eric Stratton <strattonericj@gmail.com>
2020-10-23 10:14:19 -0700

    fill in blank question prompts

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,17 @@ $(document).ready(function () {
     const q5 = parseInt($("#select3").val());
 
     const result = add(q1, q2, q3, q4, q5)
-    $("#test").text(result);
+    // $("#test").text(result);
+
+    if (result >= 14) {
+      $('#result1').show();
+    } else if (result < 13 && result > 7) {
+      $('#result2').show();
+    } else {
+      $('#result3').show();
+    }
+
+
+
   });
 });
